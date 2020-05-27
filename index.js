cb4abd1483c1d393f02960eb48e3abbcf8d14845
connor <vbrookesy@hotmail.co.uk>
2020-05-27 15:32:11 +0100

    adding exports

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to my website')
+})
+app.get('/page2', (req, res) => {
+    res.send('I am the second page')
+})
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000')
+})
---
+++ openWeatherMap.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch')
+const fs = require('fs');
+const APPID= "224566a483405adb54b0ecf2dbbce14a"
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${APPID}`;
+
+exports.getWeather = async() => {
+    let data = await fetch(url)
+   
+    let JSObject = await data.json()
+
+    //fs.writeFileSync('./data.json', JSON.stringify(JSObject))
+
+    return JSObject
+
+}
