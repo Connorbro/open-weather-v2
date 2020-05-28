d52c3effc53406e19c0f4a82afd584e84680fe4e
connor <vbrookesy@hotmail.co.uk>
2020-05-28 11:55:55 +0100

    Hbs layouts

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.hbs', hbs({
     defaultLayout: 'layout',
     extname: 'hbs'
-}))
+}));
+app.set('view engine', '.hbs');
 
 app.get('/', async(req, res) => {
     
