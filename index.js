6252935fe32081e621623b41cf341d4099f09fe7
connor <vbrookesy@hotmail.co.uk>
2020-05-28 11:46:06 +0100

    Hbs

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const app = express();
 
 require('dotenv').config();
 
-const getWeather = require('./openWeatherMap');
+const openWeatherMap = require('./lib/openWeatherMap');
 
 app.get('/', async(req, res) => {
     
-    let data = await getWeather();
+    let data = await openWeatherMap.getWeather();
+ 
     res.send(data)
 });
-
-app.get('/page2', (req, res) => {
-    res.send('I am the second page')
-})
 
 app.listen(3000, ()=> {
     console.log('Listening on port 3000')
