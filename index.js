5bff58488a2a6026dca93753a20087f3f5f1bc5f
connor <vbrookesy@hotmail.co.uk>
2020-05-28 11:53:07 +0100

    Hbs layouts

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const hbs = require('express-handlebars');
+const express = require('express'); // npm i express
+const hbs = require('express-handlebars'); //npm i express-handlebars
+const path = require('path'); //built in module
 const app = express();
 
 require('dotenv').config();
 
 const openWeatherMap = require('./lib/openWeatherMap');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: 'hbs'
+}))
+
 app.get('/', async(req, res) => {
     
     let data = await openWeatherMap.getWeather();
- 
     res.send(data)
 });
 
